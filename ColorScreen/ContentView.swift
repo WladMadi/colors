a11e920bfb934a728d207d4a2cb4be3660b26453
Wlad Madi <wlad@madi.space>
2024-11-05 20:48:32 +0300

    green bg

--- ColorScreen/ContentView.swift
+++ ColorScreen/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Зеленый фон")
+                .font(.largeTitle)
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
