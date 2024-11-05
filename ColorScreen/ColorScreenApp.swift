bf792f3a806e19c3b1200605f88c1a5d05bd26f5
Wlad Madi <wlad@madi.space>
2024-11-05 20:43:21 +0300

    Initial Commit

---
+++ ColorScreen/ColorScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorScreenApp.swift
+//  ColorScreen
+//
+//  Created by Влад Мади on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
